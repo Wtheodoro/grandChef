b8cd6c77fb423c180d1ebfcd1c9ce22888d9db37
Walison Teodoro <theodoro.walison@gmail.com>
2021-04-27 21:57:26 -0300

    set theme style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import Routes from './routes'
 import GlobalStyles from "./styles/GlobalStyles";
+
+import lightTheme from './styles/themes/light'
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyles />
-      <Routes />
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyles />
+        <Routes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+const lightTheme = {
+    title: 'light',
+
+    color: {
+        primary: '#FFF',
+        secondary: '#EF6C00',
+        tertiary: '#DCDCDC',
+
+        font: '#666'
+    }
+}
+
+export default lightTheme
